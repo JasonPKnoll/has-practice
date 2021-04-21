28ed1c603a1cbd7fbac01d84797b6de4d04eeb2c
Jason Knoll <jason.p.knoll@gmail.com>
2021-04-20 19:02:34 -0700

    Add customer hash

--- hash-examples.rb
+++ hash-examples.rb
@@ -1,15 +1,22 @@
 car {
-  'make' => 'Kia'
-  'model' => 'Optima'
-  'yeah' => '2018'
-  'color' => 'blue'
-  'doors' => 4
+  'make' => 'Kia',
+  'model' => 'Optima',
+  'yeah' => '2018',
+  'color' => 'blue',
+  'doors' => 4,
   'uesd' => true
 }
 
 animal {
-  'type' => 'dog'
-  'quadruped' => true
-  'biped' => false
-  'color' => 'white'
+  'type' => 'dog',
+  'quadruped' => true,
+  'biped' => false,
+  'color' => 'white',
+}
+
+customer {
+  'pronouns' => 'He/Him',
+  'needs_help' => false,
+  'products bought' => ['gum', 'flour', 'milk', 'eggs'],
+  'preferred bag' => 'paper'
 }
